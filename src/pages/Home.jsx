c6683d21d8fb8e36afdcc7e82889d0b014cce64f
lanbow93 <the.doctor1501@gmail.com>
2024-01-04 10:57:55 -0600

    made home buttons more flexible

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom'
 import { dateConverter } from '../utils/SharedFunctions'
 import useCheckUserSession from '../utils/useCheckUserSession'
+import { homeButtons } from '../utils/jsonDetails.json'
 
-function Home(props) {
+function Home() {
     useCheckUserSession()
     const storedUserDetails = sessionStorage.getItem('qrUserDetails')
     const userDetails = JSON.parse(storedUserDetails)
@@ -17,8 +19,11 @@ function Home(props) {
                 )}
             </p>
             <div className="buttonOptions">
-                <button>Assign Equipment</button>
-                <button>Review Inventory</button>
+                {homeButtons.map((option) => (
+                    <Link to={option[1]} key={option[1] + option[2]}>
+                        <button>{option[0]}</button>
+                    </Link>
+                ))}
             </div>
         </div>
     )
