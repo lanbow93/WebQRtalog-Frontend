f53b1f7d55cfdebba95f1790bac98e92a10dccd2
lanbow93 <the.doctor1501@gmail.com>
2024-01-04 10:40:44 -0600

    home page styled and button hover effects added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
+import { dateConverter } from '../utils/SharedFunctions'
 import useCheckUserSession from '../utils/useCheckUserSession'
 
 function Home(props) {
     useCheckUserSession()
+    const storedUserDetails = sessionStorage.getItem('qrUserDetails')
+    const userDetails = JSON.parse(storedUserDetails)
+    const { badgeName, timeStamp } = userDetails
+
     return (
         <div className="home">
-            <h1>Home</h1>
+            <h1>Welcome {badgeName}</h1>
+            <h3>Login Renewal:</h3>
+            <p>
+                {dateConverter(
+                    new Date(timeStamp).getTime() + 2 * 60 * 60 * 1000
+                )}
+            </p>
+            <div className="buttonOptions">
+                <button>Assign Equipment</button>
+                <button>Review Inventory</button>
+            </div>
         </div>
     )
 }
